904ece36ddbaeedb3d0a48e913a98abc89ef7a47
Brian Cullinan <megamindbrian@gmail.com>
2018-02-16 14:40:37 -0700

    editing color schemes

--- src/app/deals/components/coupon/coupon.component.ts
+++ src/app/deals/components/coupon/coupon.component.ts
@@ -12,6 +12,48 @@ export class CouponComponent implements OnInit {
     @Input() lat: number = 51.678418;
     @Input() lng: number = 7.809007;
 
+    colorSchemes: Array<string> = [
+        'Blue sunset',
+        'Classic and Retro',
+        'Shimmering blues and greens',
+        'Sunset over a swamp',
+        'Mediteranean blues',
+        'Exotic orchids',
+        'Sophisticated and calm',
+        'Magentas and yellows',
+        'Blue mountain peaks and clouds',
+        'Orange sunset',
+        'Vintage 1950s',
+        'Vivid and retro',
+        'Ornametal reds and yellows',
+        'Turqoise and red',
+        'Blue sundew',
+        'Classic 70s',
+        'Gothic architecture',
+        'Fiery red landscape',
+        'Urban skyline',
+        'Natural elegance',
+        'Summer blueberries',
+        'On the dock of the bay',
+        'Earthy greens',
+        'Old car and blue jeans',
+        'Berries galore',
+        'Refreshing and invigorating',
+        'Serene and relaxing',
+        'Aqua greens',
+        'Summer vacation',
+        'Twilight moon',
+        'Swiss meadows',
+        'Tropical wildlife',
+        'Child\'s play',
+        'Sunset in Hamburg',
+        'Lemon in water',
+        'Inspirational and romantic',
+        'Riverside London',
+        'Black and bright orange',
+        'Indian fields'
+    ]
+
     constructor(public menuService: MenuService) { }
 
     ngOnInit() {
